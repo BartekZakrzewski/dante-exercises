1e7ebe5e5baf822f2ad785a0fc1f4ecb45fd1688
Bartosz Zakrzewski <78502440+BartekZakrzewski@users.noreply.github.com>
2025-10-27 13:06:25 +0100

    Final solution -- passed tests

--- main.c
+++ main.c
@@ -1,66 +1,60 @@
 #include <stdio.h>
-#include <string.h>
-#define MAX_LEN 200
+#define SIZE_A 10
+#define SIZE_B 2
 
-int add(const char* number1, const char* number2, char* result, int size);
-int validate(const char * number);
-
-void find_new_line(char *l);
+int A[SIZE_A][SIZE_A], B[SIZE_B][SIZE_B];
+int check_matrix(int A[SIZE_A][SIZE_A], int B[SIZE_B][SIZE_B], int x, int y);
 
 int main() {
-    char n1[MAX_LEN + 1], n2[MAX_LEN + 1], _add[2 * MAX_LEN];
-    printf("Podaj pierwsza liczbe: ");
-    if (fgets(n1, MAX_LEN, stdin) == NULL) {
-        return 1;
-    }
-    find_new_line(n1);
-    printf("Podaj druga liczbe: ");
-    if (fgets(n2, MAX_LEN, stdin) == NULL) {
-        return 1;
-    }
-    find_new_line(n2);
-
-    int _res = add(n1, n2, _add, MAX_LEN);
-    if (_res == 1) {}
-    printf("Add: %s\n", _add);
-
-    return 0;
-}
-
-int add(const char* number1, const char* number2, char* result, int size) {
-    unsigned long long l1 = strlen(number1) - 1;
-    unsigned long long l2 = strlen(number2) - 1;
-    if (size <= 0) return 1;
-    char sum[2 * MAX_LEN];
-    int sgn1 = *number1 == '-' ? -1 : 1, sgn2 = *number2 == '-' ? -1 : 0;
-    int carry = 0, i = 0;
-    while (l1 > 0 && l2 > 0) {
-        int _sum = sgn1*(int)*(number1 - 1 + l1--) + sgn2*(int)*(number2 - 1 + l2--) + carry;
-        carry = _sum % 10;
-        _sum /= 10;
-        *(sum + i++) = (char)carry;
-        *(sum + i++) = (char)_sum;
-        l1--;l2--;
-    }
-    if (strlen(sum) > size) return 2;
-    for (int j = 0; j < i; j++) {
-        *(result + j) = *(sum + i - j);
-    }
-    return 0;
-}
-
-void find_new_line(char *l) {
-    int new_line = 0;
-    while (*l != '\0') {
-        if (*l == '\n') {
-            *l = '\0';
-            new_line = 1;
-            break;
+    int _res[(SIZE_A - 1) * (SIZE_A - 1)][2];
+    printf("Podaj liczby:\n");
+    for (int i = 0; i < SIZE_A; i++) {
+        for (int j = 0; j < SIZE_A; j++) {
+            int n = scanf("%d", (*(A + i) + j));
+            if (n != 1) {
+                printf("Incorrect input\n");
+                return 1;
+            }
         }
-        l++;
     }
-    if (!new_line) {
-        int _char;
-        while ((_char = getchar()) != '\n' || _char != '\0');
+    printf("Podaj liczby:\n");
+    for (int i = 0; i < SIZE_B; i++) {
+        for (int j = 0; j < SIZE_B; j++) {
+            int n = scanf("%d", (*(B + i) + j));
+            if (n != 1) {
+                printf("Incorrect input\n");
+                return 1;
+            }
+        }
     }
+    // printf("\nB:\n%d %d\n%d %d\n:B\n", *(*(B + 0) + 0), *(*(B + 0) + 1), *(*(B + 1) + 0), *(*(B + 1) + 1));
+    int res = 0, _index = 0;
+    for (int i = 0; i < SIZE_A - SIZE_B + 1; i++) {
+        for (int j = 0; j < SIZE_A - SIZE_B + 1; j++) {
+            // printf("\n%d %d", i, j);
+            if (check_matrix(A, B, i, j)) {
+                // printf(" TRUE\n");
+                res++;
+                _res[_index][0] = j;
+                _res[_index][1] = i;
+                _index++;
+            }
+        }
+    }
+    // printf("\nB:\n%d %d\n%d %d\n:B\n", *(*(B + 0) + 0), *(*(B + 0) + 1), *(*(B + 1) + 0), *(*(B + 1) + 1));
+
+    printf("%d\n", res);
+    for (int i = 0; i < _index; i++) {
+        printf("%d %d\n", _res[i][0], _res[i][1]);
+    }
+
+    return 0;
+}
+
+int check_matrix(int A[SIZE_A][SIZE_A], int B[SIZE_B][SIZE_B], int x, int y) {
+    // printf("\n%d %d\n%d %d\n%d %d\n", x, y, *(*(A + x) + y), *(*(A + x + 1) + y), *(*(A + x) + y + 1), *(*(A + x + 1) + y + 1));
+    return (*(*(A + x) + y) == *(*(B + 0) + 0)
+        && *(*(A + x + 1) + y) == *(*(B + 1) + 0)
+        && *(*(A + x) + y + 1) == *(*(B + 0) + 1)
+        && *(*(A + x + 1) + y + 1) == *(*(B + 1) + 1));
 }
