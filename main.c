916e153db128bee0ecff8138ba003049a4b46fde
Bartosz Zakrzewski <78502440+BartekZakrzewski@users.noreply.github.com>
2025-10-28 12:38:31 +0100

    Template

--- main.c
+++ main.c
@@ -1,31 +1,6 @@
 #include <stdio.h>
 
-int matrix[15][15];
-int count[31];
-
 int main() {
-    printf("Podaj liczby:\n");
-    for (int i = 0; i < 15; i++) {
-        for (int j = 0; j < 15; j++) {
-            int n = scanf("%d", &matrix[i][j]);
-            if (!n) {
-                printf("incorrect input\n");
-                return 1;
-            }
-            if (matrix[i][j] < 0 || matrix[i][j] >= 30) {
-                printf("value out of range\n");
-                return 2;
-            }
-            count[matrix[i][j]]++;
-        }
-    }
-
-    for (int i = 0; i < 15; i++) {
-        for (int j = 0; j < 15; j++) {
-            printf("%d ", count[matrix[i][j]]);
-        }
-        printf("\n");
-    }
 
     return 0;
 }
