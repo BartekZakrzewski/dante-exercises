68555ee05b8480fadfb20478a992d190654437c9
Bartosz Zakrzewski <78502440+BartekZakrzewski@users.noreply.github.com>
2025-11-04 11:11:47 +0100

    Template

--- main.c
+++ main.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
 
-int t[2000];
-int l[2000];
-
 int main() {
-    int a, b;
-    printf("Podal liczbe: ");
-    scanf("%d", &a);
-    printf("Podal liczbe: ");
-    scanf("%d", &b);
-    int _i = 0;
-    while (b > 0) {
-        l[_i] = b % 10;
-        b /= 10;
-        t[_i] = a * l[_i];
-        _i++;
-    }
-    int indent = _i - 1;
-    for (int i = 0; i < _i; i++) {
-        printf("%d\n", t[i]);
-    }
+
 
     return 0;
 }
