9cf267d1c0a8f4115cf3bd4e7f0454733388749b
Bartosz Zakrzewski <78502440+BartekZakrzewski@users.noreply.github.com>
2025-11-05 22:50:45 +0100

    Final solution -- passed tests

--- main.c
+++ main.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 int main() {
-    srand(time(NULL));
-    int target = rand() % 101;
-    int input;
-    for (int i = 0; i < 10; i++) {
-        printf("Zgadnij: ");
-        scanf("%d", &input);
-        if (input == target) {
-            printf("wygrales\n");
-            return 0;
-        }
-
-        if (input < target) {
-            printf("za malo\n");
-        } else {
-            printf("za duzo\n");
-        }
+    double sum = 0;
+    int n = 1;
+    while (n*(n + 1) <= 10000) {
+        sum += (double)1/(double)(n*(n + 1)); n++;
     }
-    printf("przegrales\n");
+    printf("%d %.4f\n", n-1, sum);
+
     return 0;
 }
